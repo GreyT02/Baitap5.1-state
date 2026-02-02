601bf03cefe810600ddb22497278634cd0c4df91
Ly Van Thuy <lythuy2k5@gmail.com>
2026-02-02 11:34:21 +0700

    Buoi 5 - React Native useState background app

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
 export default function App() {
+  // Tạo state background
+  const [backgroundColor, setBackgroundColor] = useState("green");
+
+  // Hàm đổi màu nền
+  const changeBackground = (color) => {
+    setBackgroundColor(color);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor }]}>
+      <StatusBar barStyle="light-content" />
+
+      <ColorButton title="GREEN" color="green" onPress={changeBackground} />
+      <ColorButton title="BLUE" color="blue" onPress={changeBackground} />
+      <ColorButton title="BROWN" color="brown" onPress={changeBackground} />
+      <ColorButton title="YELLOW" color="yellow" onPress={changeBackground} />
+      <ColorButton title="RED" color="red" onPress={changeBackground} />
+      <ColorButton title="BLACK" color="black" onPress={changeBackground} />
     </View>
+  );
+}
+function ColorButton({ title, color, onPress }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: color }]}
+      onPress={() => onPress(color)}
+    >
+      <Text
+        style={[
+          styles.buttonText,
+          { color: color === "yellow" ? "black" : "white" },
+        ]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  button: {
+    paddingVertical: 15,
+    marginVertical: 8,
+    borderRadius: 6,
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
